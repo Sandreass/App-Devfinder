b8adec43fb6879bac4f0a3b811493b823050e42d
Andrei Stroianetki <sandreasweb@gmail.com>
2024-10-14 15:00:56 +0200

    adding userstat component

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { GithubUser } from "types";
+
+export const defaultUser: GithubUser = {
+  login: "Sandreass",
+  id: 144009287,
+  avatar_url: "https://avatars.githubusercontent.com/u/144009287?v=4",
+  name: "Andreas St",
+  company: "dci digital career institute",
+  blog: "andreasst.com",
+  location: "Reutlingen",
+  bio: "Junior Frontend🚀",
+  public_repos: 15,
+  followers: 6,
+  following: 5,
+  created_at: "2023-09-04T09:14:02Z",
+};
