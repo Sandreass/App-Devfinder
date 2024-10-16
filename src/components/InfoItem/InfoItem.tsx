1fdca8eb31e732c2d7934353d6ce29f8f33f3cf9
Andrei Stroianetki <sandreasweb@gmail.com>
2024-10-16 11:55:26 +0200

    adding infoitem content

--- src/components/InfoItem/InfoItem.tsx
+++ src/components/InfoItem/InfoItem.tsx
@@ -1,9 +1,37 @@
-import styles from './InfoItem.module.scss';
+import React from "react";
+import styles from "./InfoItem.module.scss";
 
-interface InfoItemProps { }
+interface InfoItemProps {
+  icon: React.ReactNode;
+  text?: string | null;
+  isLink?: boolean;
+}
 
-export const InfoItem = ({ }: InfoItemProps) => (
-  <div className={styles.infoItem}>
-    InfoItem Component
-  </div>
-);
+export const InfoItem = ({ icon, isLink, text }: InfoItemProps) => {
+  const currentText = text || "No information provided";
+  let currentHref= '';
+
+  if (isLink) {
+    currentHref = text && text.startsWith("http") ? text : `https://${text}`;
+  }
+
+  return (
+    <div className={`${styles.infoItem}${text ? '' : ` ${styles.empty}`}`}>
+      {icon}
+      <div>
+        {isLink && text ? (
+          <a
+            href={currentHref}
+            target="_blank"
+            rel="noreferrer"
+            className={styles.link}
+          >
+            currentText
+          </a>
+        ) : 
+          currentText
+        }
+      </div>
+    </div>
+  );
+};
