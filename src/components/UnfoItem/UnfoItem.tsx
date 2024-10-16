4b9e55fc14d6ddb68bdbdfae959593b5716c5b01
Andrei Stroianetki <sandreasweb@gmail.com>
2024-10-16 10:55:09 +0200

    adding infoitem component

---
+++ src/components/UnfoItem/UnfoItem.tsx
@@ -0,0 +1,9 @@
+import styles from './UnfoItem.module.scss';
+
+interface UnfoItemProps { }
+
+export const UnfoItem = ({ }: UnfoItemProps) => (
+  <div className={styles.unfoItem}>
+    UnfoItem Component
+  </div>
+);
